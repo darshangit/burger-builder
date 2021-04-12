064f5b84aa3f396e5c6356f313fde176fc6ca760
Darshan Satya <darshans1989@gmail.com>
2021-04-12 12:28:57 -0400

    Burger initial building

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Aux from "./hoc/Aux";
 
 function App() {
   return (
     <Aux>
       <Layout>
-        <p>Test</p>
+        <BurgerBuilder />
       </Layout>
     </Aux>
   );
---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,5 @@
+const Burger = (props) => {
+  return <div></div>;
+};
+
+export default Burger;
