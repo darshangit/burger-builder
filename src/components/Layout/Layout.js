fc656f76d1943c3d0bc1a591e9c6d3c16dd725fd
Darshan Satya <darshans1989@gmail.com>
2021-04-15 20:41:21 -0400

    Backdrop slide out

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,27 @@
-import React from "react";
+import { Component } from "react";
 import Aux from "../../hoc/Aux";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 
-const Layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
 
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,17 +1,27 @@
+import Aux from "../../../hoc/Aux";
 import Logo from "../../Logo/Logo";
+import BackDrop from "../../UI/Backdrop/Backdrop";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import styles from "./SideDrawer.module.css";
 
 const SideDrawer = (props) => {
+  let attachedClasses = [styles.SideDrawer, styles.Close];
+
+  if (props.open) {
+    attachedClasses = [styles.SideDrawer, styles.Open];
+  }
   return (
-    <div className={styles.SideDrawer}>
-      <div className={styles.Logo}>
-        <Logo />
+    <Aux>
+      <BackDrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(" ")}>
+        <div className={styles.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
