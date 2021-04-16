25678d58f037ec599c4f82b64c00b93c922e407d
Darshan Satya <darshans1989@gmail.com>
2021-04-15 20:13:20 -0400

    Initial Side Drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 
 const Layout = (props) => (
   <Aux>
-    <Toolbar/>
+    <Toolbar />
+    <SideDrawer />
+
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,16 @@
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import styles from "./SideDrawer.module.css";
+
+const SideDrawer = (props) => {
+  return (
+    <div className={styles.SideDrawer}>
+      <Logo />
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
+  );
+};
+
+export default SideDrawer;
